f5113a38f6c1cf84988a815cdb7a6bed496fe543
alex <aguerriero@umass.edu>
2018-07-19 16:15:47 -0400

    Live run

--- screen_cap.py
+++ screen_cap.py
@@ -11,25 +11,28 @@ def average_slope_intercept(lines):
     right_lines   = [] # (slope, intercept)
     right_weights = [] # (length,)
     
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            if x2==x1:
-                continue # ignore a vertical line
-            slope = (y2-y1)/(x2-x1)
-            intercept = y1 - slope*x1
-            length = np.sqrt((y2-y1)**2+(x2-x1)**2)
-            if slope < 0: # y is reversed in image
-                left_lines.append((slope, intercept))
-                left_weights.append((length))
-            else:
-                right_lines.append((slope, intercept))
-                right_weights.append((length))
-    
-    # add more weight to longer lines    
-    left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
-    right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
-    
-    return left_lane, right_lane # (slope, intercept), (slope, intercept)
+    try:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                if x2==x1:
+                    continue # ignore a vertical line
+                slope = (y2-y1)/(x2-x1)
+                intercept = y1 - slope*x1
+                length = np.sqrt((y2-y1)**2+(x2-x1)**2)
+                if slope < 0: # y is reversed in image
+                    left_lines.append((slope, intercept))
+                    left_weights.append((length))
+                else:
+                    right_lines.append((slope, intercept))
+                    right_weights.append((length))
+        
+        # add more weight to longer lines    
+        left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
+        right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
+        
+        return left_lane, right_lane # (slope, intercept), (slope, intercept)
+    except:
+        return (1,1), (1,1)
 
 def make_line_points(y1, y2, line):
     """
@@ -64,14 +67,13 @@ def lane_lines(image, lines):
 
 def draw_lines(img, lines):
     print(lines)
-    #try:
-    for line in list(lines):
-        print("Coords" + str(line))
-        coords = line
-        cv2.line(img, line[0], line[1], [255,0, 0], 3)
-    #except:
-        
-     #   pass
+    try:
+        for line in list(lines):
+            print("Coords" + str(line))
+            coords = line
+            cv2.line(img, line[0], line[1], [255,0, 0], 3)
+    except:
+       pass
 
 
 def roi(img, vertices):
@@ -93,21 +95,25 @@ def process_image(original_image):
     draw_lines(processed_img, lanes)
     return processed_img
 
-def run_screen_capture():
+def run_screen_capture(region):
     while(True):
         start = time.time()
         image = pyautogui.screenshot(region=region)
         print("Time took: {}".format(time.time() - start))
         
-        cv2.imshow('image', cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB))
+        cv2.imshow('image', process_image(np.array(image)))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
 
 
 if __name__ == "__main__":
+    '''
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('image', 600,600)
     cv2.imshow('image', process_image(cv2.imread('car3.PNG')))
     cv2.waitKey()
     cv2.destroyAllWindows()
+    '''
+    region = (0,40, 1920, 1120)
+    run_screen_capture(region)
