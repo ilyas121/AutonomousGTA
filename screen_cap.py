81fcb7d5d52ba5636d326f16fd12c544e15682f1
alex <aguerriero@umass.edu>
2018-07-19 17:20:55 -0400

    better edges

--- screen_cap.py
+++ screen_cap.py
@@ -84,7 +84,7 @@ def roi(img, vertices):
 def process_image(original_image):
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.GaussianBlur(processed_img, (15,15), 0)
-    processed_img = cv2.Canny(processed_img, threshold1 = 50, threshold2=150)
+    processed_img = cv2.Canny(processed_img, threshold1 = 50, threshold2=100)
     vertices = np.array([[120,1131], [642,564], [1278,573], [2008, 1115]])
     processed_img = roi(processed_img, [vertices] )
     lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 150, 5)
@@ -107,7 +107,6 @@ def run_screen_capture(region):
 
 
 if __name__ == "__main__":
-    '''
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('image', 600,600)
     cv2.imshow('image', process_image(cv2.imread('car3.PNG')))
@@ -116,3 +115,4 @@ if __name__ == "__main__":
     '''
     region = (0,40, 1920, 1120)
     run_screen_capture(region)
+    '''
