31d3e96a547bf5be0506a843f0715e46402d9373
alex <aguerriero@umass.edu>
2018-07-19 16:06:08 -0400

    Finding lanes

--- screen_cap.py
+++ screen_cap.py
@@ -1,19 +1,113 @@
 import numpy as np
-from PIL import ImageGrab
 import cv2
 import time
 import pyautogui
 
-while(True):
-    start = time.time()
-    # printscreen_pil =  ImageGrab.grab(bbox=(0,40,800,640))
-    image = pyautogui.screenshot(region=(0,40,800,640))
-    # printscreen_numpy =   np.array(printscreen_pil.getdata(),dtype='uint8')\
-    #.reshape((printscreen_pil.size[1],printscreen_pil.size[0],4)) 
-     
-    print("Time took: {}".format(time.time() - start))
+region = (0,40,800,640)
+
+def average_slope_intercept(lines):
+    left_lines    = [] # (slope, intercept)
+    left_weights  = [] # (length,)
+    right_lines   = [] # (slope, intercept)
+    right_weights = [] # (length,)
     
-    cv2.imshow('image', np.array(image))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            if x2==x1:
+                continue # ignore a vertical line
+            slope = (y2-y1)/(x2-x1)
+            intercept = y1 - slope*x1
+            length = np.sqrt((y2-y1)**2+(x2-x1)**2)
+            if slope < 0: # y is reversed in image
+                left_lines.append((slope, intercept))
+                left_weights.append((length))
+            else:
+                right_lines.append((slope, intercept))
+                right_weights.append((length))
+    
+    # add more weight to longer lines    
+    left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
+    right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
+    
+    return left_lane, right_lane # (slope, intercept), (slope, intercept)
+
+def make_line_points(y1, y2, line):
+    """
+    Convert a line represented in slope and intercept into pixel points
+    """
+    if line is None:
+        return None
+    
+    slope, intercept = line
+    
+    # make sure everything is integer as cv2.line requires it
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    y1 = int(y1)
+    y2 = int(y2)
+    
+    return ((x1, y1), (x2, y2))
+
+
+def lane_lines(image, lines):
+    left_lane, right_lane = average_slope_intercept(lines)
+    
+    y1 = image.shape[0] # bottom of the image
+    y2 = y1*0.3         # slightly lower than the middle
+
+    left_line  = make_line_points(y1, y2, left_lane)
+    right_line = make_line_points(y1, y2, right_lane)
+    
+    return left_line, right_line
+
+    
+
+def draw_lines(img, lines):
+    print(lines)
+    #try:
+    for line in list(lines):
+        print("Coords" + str(line))
+        coords = line
+        cv2.line(img, line[0], line[1], [255,0, 0], 3)
+    #except:
+        
+     #   pass
+
+
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def process_image(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.GaussianBlur(processed_img, (5,5), 0)
+    processed_img = cv2.Canny(processed_img, threshold1 = 150, threshold2=175)
+    vertices = np.array([[320,1131], [842,564], [1078,573], [1808, 1115]])
+    processed_img = roi(processed_img, [vertices] )
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 100, 5)
+    
+    lanes = lane_lines(processed_img, lines)
+
+    draw_lines(processed_img, lanes)
+    return processed_img
+
+def run_screen_capture():
+    while(True):
+        start = time.time()
+        image = pyautogui.screenshot(region=region)
+        print("Time took: {}".format(time.time() - start))
+        
+        cv2.imshow('image', cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+
+if __name__ == "__main__":
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image', 600,600)
+    cv2.imshow('image', process_image(cv2.imread('car3.PNG')))
+    cv2.waitKey()
+    cv2.destroyAllWindows()
